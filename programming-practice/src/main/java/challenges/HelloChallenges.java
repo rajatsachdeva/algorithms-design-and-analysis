7333a05ade399504845f9b45fe23c95a077c6792
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2019-02-12 00:59:35 +0100

    Initial Commit

---
+++ programming-practice/src/main/java/challenges/HelloChallenges.java
@@ -0,0 +1,8 @@
+package challenges;
+
+public class HelloChallenges {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Future Challenges");
+    }
+}
---
+++ programming-practice/src/main/java/foundation/HelloFoundation.java
@@ -0,0 +1,7 @@
+package foundation;
+
+public class HelloFoundation {
+    public static void main(String[] args) {
+        System.out.println("Hello Foundation");
+    }
+}
